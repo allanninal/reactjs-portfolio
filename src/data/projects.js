d813cddf633155323ad7223b8af35e14f8b2706b
Allan Niñal <landix.ninal@gmail.com>
2023-12-28 12:59:09 +0800

    updated the projects title, image, and description

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,26 +1,26 @@
-import project1 from '../assets/project1.png';
-import project2 from '../assets/project2.png';
-import project3 from '../assets/project3.png';
+import project1 from '../assets/project1.jpg';
+import project2 from '../assets/project2.jpg';
+import project3 from '../assets/project3.jpg';
 
 const PROJECTS = [
     {
         id: 1,
-        title: 'Example React Application',
-        description: 'A react app that I build involving JS and core web dev concepts!',
-        link: 'https://github.com/allanninal/reactjs-portfolio',
+        title: 'Even or Odds',
+        description: 'A react app using the redux, reducers, and action concepts!',
+        link: 'https://github.com/allanninal/even-or-odds',
         image: project1
     },
     {
         id: 2,
-        title: 'My API',
-        description: 'A REST API that I built from scratch with GET and POST!',
-        link: 'https://github.com/allanninal/reactjs-portfolio',
+        title: 'Music Master',
+        description: 'A react app using the concept or components!',
+        link: 'https://github.com/allanninal/music-master',
         image: project2
     },
     {
         id: 3,
-        title: 'Operating Systems Final Project',
-        description: 'My unique final project for Operationg System!',
+        title: 'My Portfolio',
+        description: 'A react app that I build involving JS and core web dev concepts!',
         link: 'https://github.com/allanninal/reactjs-portfolio',
         image: project3
     }
